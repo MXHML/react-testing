ff4c745387917dc16e4db64fbeb0a0815004eac3
notperquisites <81310814+notperquisites@users.noreply.github.com>
2022-11-08 21:09:47 -0600

    2

--- src/index.js
+++ src/index.js
@@ -3,18 +3,12 @@ import ReactDOM from 'react-dom/client';
 
 const root= ReactDOM.createRoot(document.getElementById('root'));
 
-const elements = (
-  <table>
-    <tr>
-      <th>Name</th>
-    </tr>
-    <tr>
-      <td>John</td>
-    </tr>
-    <tr>
-      <td>Elsa</td>
-    </tr>
-  </table>
-);
+var text=''
+
+for(let x=0;x<100;x++){
+  text=x
+}
+
+const elements = <h1>{text}</h1>
 
 root.render(elements);
